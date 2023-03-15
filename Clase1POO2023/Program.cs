427e8fccc071f34efc341d125bd36e2d11e1b11e
Guido <guidoca_94@hotmail.com>
2023-03-15 00:10:41 -0300

    EJ POO
    EJ POO

---
+++ Clase1POO2023/Program.cs
@@ -0,0 +1,46 @@
+﻿var alumno1=new Alumno();
+alumno1.Nombre="Facundo";
+alumno1.Apellido="Perez";
+alumno1.Edad=22;
+
+var alumno2=new Alumno();
+alumno2.Nombre="Brenda";
+alumno2.Apellido="Laporte";
+alumno2.Edad=34;
+alumno1.Inscripto=false;
+alumno2.Inscripto=false;
+
+Console.WriteLine("Nombre alumno 1: " + alumno1.Nombre);
+alumno1.Inscribirse();
+Console.WriteLine($"Está inscripto? {alumno1.Inscripto}");
+Console.WriteLine($"Está inscripto? {alumno2.Inscripto}");
+Console.WriteLine("Nombre alumno 2: " + alumno2.Nombre + " " + alumno2.Apellido);
+alumno1.Cursar();
+alumno2.Cursar();
+
+class Alumno {
+    public string Nombre{get;set;}
+    public string Apellido{get;set;}
+    public int Edad{get;set;}
+
+    public bool Inscripto {get;set;}
+    public DateTime FechaNacimiento{get;set;}
+
+    public void Inscribirse(){
+        Inscripto=true;
+    }
+    public void Cursar(){
+        /*
+        if(Inscripto)
+            Console.WriteLine("Cursando");
+        else
+            Console.WriteLine("No estoy inscripto y no puedo cursar.");
+        */
+        if(Inscripto==true){
+            Console.WriteLine("Cursando");
+        }
+        else{
+            Console.WriteLine("No estoy inscripto y no puedo cursar.");
+        }
+    }
+}
